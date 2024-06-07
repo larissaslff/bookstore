636b60d572a3709bff55a4615d5a0ea431c5e1b6
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-06 21:02:08 -0300

    making the imports explicits

--- store/src/main/java/com/book/store/models/Review.java
+++ store/src/main/java/com/book/store/models/Review.java
@@ -1,7 +1,16 @@
 package com.book.store.models;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
