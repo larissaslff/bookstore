b0b40e346809a28be7f9e41539cfc76329b45708
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-06 21:53:49 -0300

    adding relationshing between book and author

--- store/src/main/java/com/book/store/models/Book.java
+++ store/src/main/java/com/book/store/models/Book.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Builder
@@ -26,5 +27,11 @@ public class Book {
 
     @OneToOne
     private Review review;
+
+    @ManyToMany
+    @JoinTable(name = "authors_books",
+            joinColumns = @JoinColumn(name="book_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id", referencedColumnName = "id"))
+    private Set<Author> authors;
 
 }
