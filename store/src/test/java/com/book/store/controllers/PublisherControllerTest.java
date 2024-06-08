9aa611cd9c1dc7a01a7cd7b51f65144c1acde189
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-08 12:52:30 -0300

    adding publisher controller

--- store/src/test/java/com/book/store/controllers/PublisherControllerTest.java
+++ store/src/test/java/com/book/store/controllers/PublisherControllerTest.java
@@ -2,20 +2,26 @@ package com.book.store.controllers;
 
 import com.book.store.models.Publisher;
 import com.book.store.services.PublisherServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class PublisherControllerTest {
@@ -26,6 +32,8 @@ class PublisherControllerTest {
     @InjectMocks
     private PublisherController publisherController;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     private MockMvc mockMvc;
 
     @BeforeEach
@@ -34,7 +42,7 @@ class PublisherControllerTest {
     }
 
     @Test
-    void shouldAllowSaveAPublisher() {
+    void shouldAllowSaveAPublisher() throws Exception {
         Publisher publisherToSave = Publisher.builder()
                 .name("A publisher name")
                 .build();
@@ -44,6 +52,12 @@ class PublisherControllerTest {
                 .build();
 
         when(publisherService.savePublisher(any(Publisher.class))).thenReturn(savedPublisher);
+
+        mockMvc.perform(post("/publishers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(publisherToSave)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is("A publisher name")));
 
     }
 }
