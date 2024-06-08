af29f885112d7a2baaf1f1c7b66b5b6a2d517305
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-08 11:44:55 -0300

    using tdd to implement save publisher method

--- store/src/main/java/com/book/store/services/PublisherService.java
+++ store/src/main/java/com/book/store/services/PublisherService.java
@@ -1,4 +1,8 @@
 package com.book.store.services;
 
+import com.book.store.models.Publisher;
+
 public interface PublisherService {
-}
+    void savePublisher(Publisher publisherToSave);
+
+    }
--- store/src/main/java/com/book/store/services/PublisherServiceImpl.java
+++ store/src/main/java/com/book/store/services/PublisherServiceImpl.java
@@ -1,4 +1,11 @@
 package com.book.store.services;
 
+import com.book.store.models.Publisher;
+
 public class PublisherServiceImpl implements PublisherService{
+
+    @Override
+    public void savePublisher(Publisher publisherToSave) {
+
+    }
 }
