98d8eba98ff170b725f5bd531a2f532693a7a7b7
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-08 17:04:18 -0300

    adding repositories

--- store/src/main/java/com/book/store/repositories/BookRepository.java
+++ store/src/main/java/com/book/store/repositories/BookRepository.java
@@ -1,6 +1,5 @@
 package com.book.store.repositories;
 
-import com.book.store.models.Author;
 import com.book.store.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- store/src/main/java/com/book/store/repositories/ReviewRepository.java
+++ store/src/main/java/com/book/store/repositories/ReviewRepository.java
@@ -1,6 +1,5 @@
 package com.book.store.repositories;
 
-import com.book.store.models.Author;
 import com.book.store.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
