d8dc77c85e7db0a89e5a7973862f0d811a647d32
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-08 12:57:46 -0300

    adding publisher controller

--- store/src/main/java/com/book/store/controllers/PublisherController.java
+++ store/src/main/java/com/book/store/controllers/PublisherController.java
@@ -1,9 +1,24 @@
 package com.book.store.controllers;
 
+import com.book.store.models.Publisher;
+import com.book.store.services.PublisherService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/publisher")
+@RequestMapping("/publishers")
 public class PublisherController {
+
+    private final PublisherService publisherService;
+
+    private PublisherController(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Publisher> registerANewPublisher(){
+
+    }
 }
