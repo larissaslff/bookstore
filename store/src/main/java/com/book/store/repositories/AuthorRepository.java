a6e220226ff64062b48414ccbe449b51e696a4be
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-08 16:16:30 -0300

    adding authorRepository

---
+++ store/src/main/java/com/book/store/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.book.store.repositories;
+
+import com.book.store.models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AuthorRepository extends JpaRepository<Author, UUID> {
+}
