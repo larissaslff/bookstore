f6f8777a520080740756334ffb8544d46d71991c
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-08 13:01:43 -0300

    adding publisher registerNewPublisher method

--- store/src/main/java/com/book/store/controllers/PublisherController.java
+++ store/src/main/java/com/book/store/controllers/PublisherController.java
@@ -2,8 +2,11 @@ package com.book.store.controllers;
 
 import com.book.store.models.Publisher;
 import com.book.store.services.PublisherService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +21,8 @@ public class PublisherController {
     }
 
     @PostMapping
-    public ResponseEntity<Publisher> registerANewPublisher(){
-
+    public ResponseEntity<Publisher> registerANewPublisher(@RequestBody Publisher publisher){
+        Publisher savedPublisher = publisherService.savePublisher(publisher);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPublisher);
     }
 }
