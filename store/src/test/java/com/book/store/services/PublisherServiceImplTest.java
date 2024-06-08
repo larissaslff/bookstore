541b552b5c0366272498f0d235a0d0567b83a525
larissaslff <110000521+larissaslff@users.noreply.github.com>
2024-06-08 11:42:34 -0300

    using tdd to implement save publisher method

---
+++ store/src/test/java/com/book/store/services/PublisherServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.book.store.services;
+
+import com.book.store.repositories.PublisherRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PublisherServiceImplTest {
+
+    @Mock
+    private PublisherRepository publisherRepository;
+
+    @InjectMocks
+    private PublisherServiceImpl publisherService;
+
+    @Test
+    void shouldSaveAPublisher() {
+        //Arrange
+
+
+        //Act
+
+
+        //Assert
+
+
+    }
+
+}
